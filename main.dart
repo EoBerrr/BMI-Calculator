aec9c8c3fd731f98fc38ca70ec4268f61106cae4
eoberrr <86076081+EoBerrr@users.noreply.github.com>
2022-09-23 07:42:03 -0300

    Add files via upload
    fix: adicionando o arquivo correto

--- main.dart
+++ main.dart
@@ -1,83 +1,172 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: "Contado os alunos", home: Home()));
+  runApp(MaterialApp(
+      home: Home()
+  ));
 }
 
 class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  int _num_alunos = 0;
-  String _mensagem = "Limite de pessoas suportadas no LAB não foi atingido";
+  TextEditingController weightController = TextEditingController () ;
+  TextEditingController heightController = TextEditingController () ;
+  String _infoText = "Informe seus dados";
+  GlobalKey<FormState> _formkey = GlobalKey<FormState>();
 
-  void _change(int increment) {
-    setState(() {
-      if (increment == -1) {
-        if (_num_alunos > 0) {
-          _num_alunos--;
-        }
-      } else if (increment == 1) {
-        _num_alunos++;
-      }
-      checkAlunos(_num_alunos);
+  void _resetFields(){
+    _formkey = GlobalKey<FormState>();
+    weightController .text = "";
+    heightController .text = "";
+    setState ((){
+      _infoText = "Infome seus dados";
     });
   }
 
-  void checkAlunos(int quantAlunos) {
-    setState(() {
-      if (quantAlunos == 36) {
-        _mensagem = "Limite de pessoas suportadas no LAB foi atingido";
-      } else if (quantAlunos < 36) {
-        _mensagem = "Limite de pessoas suportadas no LAB não foi atingido";
+  void _calculate(){
+    setState (() {
+      double weight = double.parse(weightController .text);
+      double height = double.parse(heightController .text) / 100;
+      double imc = weight / (height * height);
+
+      if (imc < 18.6){
+        _infoText = "Você está abaixo do peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.6 && imc < 24.9){
+        _infoText = "Você está no peso normal (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 25 && imc < 29.9){
+        _infoText = "Você está com sobrepeso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 30 && imc < 34.9){
+        _infoText = "Você está com Obesidade grau I (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 35 && imc < 39.9){
+        _infoText = "Você está com Obesidade grau II (${imc.toStringAsPrecision(4)})";
+      } else if (imc > 40){
+        _infoText = "Você está com Obesidade grau III (${imc.toStringAsPrecision(4)})";
       }
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Text(
-          "Alunos: $_num_alunos",
-          style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
-        ),
-        Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(15.10),
-            child: TextButton(
-              child: Text(
-                "-1",
-                style: TextStyle(fontSize: 40, color: Colors.greenAccent),
+  Widget build (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar (
+        title: const Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+        actions: <Widget> [
+          IconButton(icon: const Icon(Icons.refresh),
+            onPressed: _resetFields),
+        ],
+      ),
+      backgroundColor: Colors.black54,
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget> [
+              const Icon(Icons.personal_injury, size: 120, color: Colors.red),
+              Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                child: Container(
+                  height: 50,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Peso (Kg)",
+                      labelStyle: TextStyle(color: Colors.cyanAccent),
+                      fillColor: Colors.white12,
+                      filled: true,
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.lightGreenAccent, fontSize: 30.0),
+                    controller: weightController,
+                    validator: (value){
+                      if (value.toString().isEmpty){
+                        return "Insira o peso";
+                      }
+                      if (double.parse(value.toString()) > 500){
+                        return "Digite um valor válido";
+                      }
+                      if (double.parse(value.toString()) < 0){
+                        return "Digite um valor válido";
+                      }
+                      if (num.tryParse(value.toString()) == null){
+                        return "Digite um valor válido";
+                      }
+                    },
+                  ),
+                ),
               ),
-              onPressed: () {
-                _change(-1);
-              },
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(15.10),
-            child: TextButton(
-              child: Text(
-                "+1",
-                style: TextStyle(fontSize: 40, color: Colors.greenAccent),
+              Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                child: Container(
+                  height: 50,
+                  child: TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: "Altura (cm)",
+                      labelStyle: TextStyle(color: Colors.cyanAccent),
+                      fillColor: Colors.white12,
+                      filled: true,
+                    ),
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.lightGreenAccent, fontSize: 30.0),
+                    controller: heightController,
+                    validator: (value) {
+                      if (value
+                          .toString()
+                          .isEmpty) {
+                        return "Insira a altura";
+                      }
+                      if (double.parse(value.toString()) > 300) {
+                        return "Digite um valor válido";
+                      }
+                      if (double.parse(value.toString()) < 0) {
+                        return "Digite um valor válido";
+                      }
+                      if (num.tryParse(value.toString()) == null) {
+                        return "Digite um valor válido";
+                      }
+                    }
+                  ),
+                ),
               ),
-              onPressed: () {
-                _change(1);
-              },
-            ),
+              Padding (padding: EdgeInsets.only(top:10, bottom: 10),
+                child: Container(
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.red,
+                      elevation: 20,
+                      shadowColor: Colors.indigo,
+                    ),
+                    child: const Text('Calcular IMC'),
+                    onPressed: (){
+                      if (_formkey.currentState!.validate())
+                        _calculate();
+                    },
+                  ),
+                ),
+              ),
+
+              Padding(padding: EdgeInsets.only(top:10, bottom: 10),
+                child: Container(
+                  height: 100,
+                  child: Text(
+                    _infoText,
+                    style: const TextStyle(color: Colors.lightGreenAccent, fontSize: 30.0),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              )
+            ],
           ),
-        ]),
-        Text(
-          "$_mensagem",
-          style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
         ),
-      ],
+
+    ),
     );
   }
 }
